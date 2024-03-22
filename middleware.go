ef2407f0a8c52d59c71f229e7fb74fa67a25b3f0
aliasboink <adrianlaurb@gmail.com>
2024-03-22 11:57:58 +0200

    Remove silly logging

--- middleware.go
+++ middleware.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"log"
 	"net/http"
 	"strings"
 )
@@ -11,8 +10,6 @@ type authedHandler func(http.ResponseWriter, *http.Request)
 func (cfg *apiConfig) middlewareAuth(handler authedHandler) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		apikeyString := strings.TrimPrefix(r.Header.Get("Authorization"), "ApiKey ")
-		log.Println("Haha", apikeyString)
-		log.Println("Haha2", cfg.ApiKey)
 		if apikeyString != cfg.ApiKey {
 			respondWithError(w, 401, "Unauthorized!")
 			return
